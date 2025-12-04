031356038915d917a0e624e358a61343e86dc161
Ivanxx16 <sanchezanthony842@gmail.com>
2025-12-04 08:54:04 -0500

    Metodo Scraping para obtener datos de contribuyente del SRI

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express')
+const cors = require('cors')  // ← NUEVO
 const app = express()
-const port = 3000
-const {obtenerDatosRuc} = require('./ScrapingSRI.js');
+const port = 3001
+const { obtenerDatosRuc } = require('./ScrapingSRI.js')
+
+app.use(cors())  // ← NUEVO: Habilitar CORS
 
 app.get('/consultar', async (req, res) => {
-
-    //http://localhost:3000/consultar?ruc=1150575338001
-    const ruc = req.query.ruc;
-    const data = await obtenerDatosRuc(ruc);
-    console.log(data);
-    res.send(data);
+    const ruc = req.query.ruc
+    const data = await obtenerDatosRuc(ruc)
+    console.log(data)
+    res.send(data)
 })
 
 app.listen(port, () => {
